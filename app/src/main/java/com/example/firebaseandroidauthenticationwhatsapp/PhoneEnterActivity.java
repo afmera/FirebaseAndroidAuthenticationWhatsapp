e9c473479b945e8a28be633c6c3354375986b193
afmera <felipemera@unimayor.edu>
2019-09-25 10:13:49 -0500

    Initial commit

---
+++ app/src/main/java/com/example/firebaseandroidauthenticationwhatsapp/PhoneEnterActivity.java
@@ -0,0 +1,14 @@
+package com.example.firebaseandroidauthenticationwhatsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhoneEnterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_enter);
+    }
+}
